e034751f881db8d411365fe6fa9ca433b1fcdaa2
Teemu Erkkola <teemu.erkkola@gofore.com>
2015-12-02 09:18:46 +0200

    YH-738: work on data model conversion

--- modules/ckanext-ytp-main/ckanext/ytp/dataset/public/javascript/package.js
+++ modules/ckanext-ytp-main/ckanext/ytp/dataset/public/javascript/package.js
@@ -1,8 +1,8 @@
 var SaveAsDraft = function(){
-    document.querySelector('#field-state').value = 'draft';
+//    document.querySelector('#field-state').value = 'draft';
 }
 
 var SaveAsActive = function(){
-    document.querySelector('#field-state').value = 'active';
+//    document.querySelector('#field-state').value = 'active';
 }
 
